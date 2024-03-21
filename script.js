f6757edeabfd05118a52553f1b7190f3e4573f3b
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2024-03-20 21:38:32 -0300

    arquivos iniciais do projeto

---
+++ script.js
@@ -0,0 +1,43 @@
+voidText = document.getElementById("box1").value;
+console.log(voidText);
+switch (voidText) {
+    case 'a':
+        'a' = 'ai'
+        break;
+    
+    case 'e' ='enter':
+        
+        break;    
+    
+    case 'i' = 'imes':
+        
+        break;    
+    
+    case 'o' = 'ober':
+        
+        break;    
+    
+    case 'u' ='ufat':
+        
+        break;
+    default:
+        break;
+}
+
+getTextArea = document.getElementById("box1").innerHTML = '';
+content = getTextArea.value;
+
+
+function crypto() {
+    
+    a = ai;
+    e = enter;
+    i = imes;
+    o = ober;
+    u = ufat;
+}
+
+function decripto() {
+    
+}
+
